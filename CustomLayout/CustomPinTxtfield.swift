124dc67a26cc1db772b02fa0ef086a8b9abb8695
jonysingla <jony.singla19@gmail.com>
2019-07-16 18:02:55 +0530

    OTP

---
+++ CustomLayout/CustomPinTxtfield.swift
@@ -0,0 +1,21 @@
+//
+//  CustomPinTxtfield.swift
+//  LoginFlow
+//
+//  Created by Jony on 16/07/19.
+//  Copyright © 2019 Jony. All rights reserved.
+//
+
+import UIKit
+
+class CustomPinTxtfield: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
